f1e9592440e4ed25f4d24f77363d464fd3f2206a
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-01-27 22:24:53 -0800

    featL push v1

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const betterEval = require("better-eval");
+const fs = require("fs");
 
-console.log(betterEval("1+1"));
+const fv = (fileName, variables) => {
+  const raw = fs.readFileSync(__dirname + "/" + fileName, "utf-8");
+
+  const evaled = raw.replace(/{(.*?)}/g, (expression) => {
+    const withoutCurly = expression.substring(1, expression.length - 1);
+
+    return betterEval(withoutCurly, variables);
+  });
+
+  return evaled;
+};
+
+module.exports = fv;
