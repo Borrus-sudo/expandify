e5e7c2ec9ae0b8cd14d8b58aa49c8c9121651641
(TS/JS).Gandalf(he/him) <58482194+Borrus-sudo@users.noreply.github.com>
2022-01-31 21:09:36 +0530

    refactor: improve regex and refactor

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 const expandify = (fileName, variables) => {
   const raw = fs.readFileSync(fileName, "utf-8");
 
-  const evaled = raw.replace(/{(.*)}/g, (expression) => {
-    const withoutCurly = expression.substring(1, expression.length - 1);
+  const evaled = raw.replace(/{[\s\S]*}/g, (expression) => {
+    const withoutCurly = expression.substring(1,-1);
 
     return betterEval(withoutCurly, variables);
   });
